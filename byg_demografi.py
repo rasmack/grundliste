392511d0285a77ac94703f3dbfe415982a683a86
THINKPAD\Rasmus <rmac@nykredit.dk>
2025-11-27 22:47:21 +0100

    Så har vi rådata

--- byg_demografi.py
+++ byg_demografi.py
@@ -11,14 +11,14 @@ url = "https://api.statbank.dk/v1/data"
 # Alder
 
 # Parametre / payload
-payload = {
+payload_alder_køn = {
    "table": "FOLK1AM",
    "format": "CSV",
    "variables": [
       {
          "code": "KØN",
          "values": [
-            "TOT"
+            "*"
          ]
       },
       {
@@ -36,15 +36,117 @@ payload = {
    ]
 }
 
-# POST request til API
-response = requests.post(url, json=payload)
+payload_herkomst = {
+   "table": "FOLK1E",
+   "format": "CSV",
+   "variables": [
+      {
+         "code": "KØN",
+         "values": [
+            "TOT"
+         ]
+      },
+      {
+         "code": "ALDER",
+         "values": [
+            "IALT"
+         ]
+      },
+      {
+         "code": "OMRÅDE",
+         "values": [
+            "350"
+         ]
+      },
+      {
+         "code": "HERKOMST",
+         "values": [
+            "*"
+         ]
+      }
+   ]
+}
 
-# Tjek om requesten lykkedes
-if response.status_code == 200:
-    # Læs CSV-data direkte ind i en Pandas DataFrame
-    df = pd.read_csv(StringIO(response.text), sep=";")
-    print(df.head())
-else:
-    print(f"Fejl ved hentning: {response.status_code}")
-    print(response.text)
+payload_uddannelse = {
+   "table": "HFUDD11",
+   "format": "CSV",
+   "variables": [
+      {
+         "code": "KØN",
+         "values": [
+            "TOT"
+         ]
+      },
+      {
+         "code": "ALDER",
+         "values": [
+            "TOT"
+         ]
+      },
+      {
+         "code": "BOPOMR",
+         "values": [
+            "350"
+         ]
+      },
+      {
+         "code": "HERKOMST",
+         "values": [
+            "TOT"
+         ]
+      },
+      {
+         "code": "HFUDD",
+         "values": [
+            "*"
+         ]
+      }
+   ]
+}
+
+payload_postnr = {
+   "table": "POSTNR1",
+   "format": "CSV",
+   "variables": [
+      {
+         "code": "PNR20",
+         "values": [
+            "*"
+         ]
+      },
+      {
+         "code": "KØN",
+         "values": [
+            "TOT"
+         ]
+      },
+      {
+         "code": "ALDER",
+         "values": [
+            "IALT"
+         ]
+      }
+   ]
+}
+
+def hentdata(payload):
+    # POST request til API
+    response = requests.post(url, json=payload)
+
+    # Tjek om requesten lykkedes
+    if response.status_code == 200:
+        # Læs CSV-data direkte ind i en Pandas DataFrame
+        df = pd.read_csv(StringIO(response.text), sep=";")
+        print(df.head())
+    else:
+        print(f"Fejl ved hentning: {response.status_code}")
+        print(response.text)
+    return df
+
+df_alder = hentdata(payload_alder_køn)
+df_herkomst = hentdata(payload_herkomst)
+df_uddannelse = hentdata(payload_uddannelse)
+df_postnr = hentdata(payload_postnr) # Tager ikke højde for, at postnumre ikke følger kommunegrænser
+
+
 pass
