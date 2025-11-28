7629369bfb42c0365d2a1ecdd82c19e25dc73d1a
Rasmus Mackeprang <rmacke@danskebank.dk>
2025-11-28 08:12:37 +0100

    Requirements - lidt data wrangling

--- byg_demografi.py
+++ byg_demografi.py
@@ -143,10 +143,29 @@ def hentdata(payload):
         print(response.text)
     return df
 
+# Rå indhentning fra Statistikbanken
 df_alder = hentdata(payload_alder_køn)
 df_herkomst = hentdata(payload_herkomst)
 df_uddannelse = hentdata(payload_uddannelse)
 df_postnr = hentdata(payload_postnr) # Tager ikke højde for, at postnumre ikke følger kommunegrænser
 
+# Bearbejdning
+
+# Kønsfordeling
+df_køn = df_alder[(df_alder['ALDER']=='Alder i alt')&(df_alder['KØN']!='I alt')][['KØN','INDHOLD']]
+df_køn['Brøkdel']=df_køn['INDHOLD']/df_køn['INDHOLD'].sum()
+df_køn['Køn']=df_køn['KØN'].map({'Mænd':'Mand','Kvinder':'Kvinde'})
+df_køn = df_køn[['Køn','Brøkdel']]
+
+# Aldersfordeling
+aldersgruppering = [18,25,30,40,50,60,70,80,200]
+group_labels = ['18-24', '25-29', '30-39', '40-49', '50-59', '60-69', '70-79','80+']
+
+df_alder = df_alder[df_alder['KØN']!='I alt'][['ALDER','INDHOLD']]
+df_alder = df_alder[df_alder['ALDER']!='Alder i alt']
+df_alder['Alder']=df_alder['ALDER'].str.slice(stop=-2).astype(int)
+df_alder['Aldersgruppe']=df_alder['Alder']
+df_alder['aldersgruppe'] = pd.cut(df_alder['Alder'], bins=aldersgruppering, right=False, labels=group_labels)
+print(df_alder[['Alder','INDHOLD']])
 
 pass
